de478b1a6cf75c3a0c02f339c721f63de8c6cb13
marcootom <marcootomasi@gmail.com>
2020-08-31 19:30:54 +0200

    Terminata pagina Home Utente

---
+++ app/src/main/java/com/example/valium/Ricette.java
@@ -0,0 +1,14 @@
+package com.example.valium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ricette extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ricette);
+    }
+}

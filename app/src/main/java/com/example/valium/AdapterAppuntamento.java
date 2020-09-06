6da4fd983efba92117d8ba07a3aae0c896c0b95f
marcootom <marcootomasi@gmail.com>
2020-09-06 13:23:54 +0200

    Revert "Non so creare un adapter"
    This reverts commit 605ce2f3d7116fab041481773f90aa031324b7fc.

--- app/src/main/java/com/example/valium/AdapterAppuntamento.java
+++
@@ -1,4 +0,0 @@
-package com.example.valium;
-
-public class AdapterAppuntamento {
-}
--- app/src/main/java/com/example/valium/ListaPrenotazioni.java
+++ app/src/main/java/com/example/valium/ListaPrenotazioni.java
@@ -1,27 +1,14 @@
 package com.example.valium;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListaPrenotazioni extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_prenotazioni);
-
-        ArrayList<String> AC = new ArrayList<>();
-        AC.add("fgte");
-        AC.add("darw");
-        ListView sw = findViewById(R.id.listaAppuntamenti);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.card_appuntamento, AC);
-        sw.setAdapter(adapter);
-
     }
 }

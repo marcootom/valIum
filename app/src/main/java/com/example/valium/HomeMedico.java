94a0892408bdfe073e5e5fc9edb9f1063306f0cf
marcootom <marcootomasi@gmail.com>
2020-09-10 12:40:25 +0200

    Modifiche layout

--- app/src/main/java/com/example/valium/HomeMedico.java
+++ app/src/main/java/com/example/valium/HomeMedico.java
@@ -1,19 +1,37 @@
 package com.example.valium;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class HomeMedico extends AppCompatActivity {
     TextView benvenuto;
+    public CardView listaPazienti, listaPrenotazioni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_medico);
+        listaPazienti = findViewById(R.id.bottoneListaPazienti);
+        listaPrenotazioni = findViewById(R.id.bottoneListaPrenotazioni);
+
         benvenuto = findViewById(R.id.benvenutomedico);
         String s = "Benvenuto, Dottor " + MappaUtenti.recuperaUtente(MappaUtenti.getUtenteAttuale()).getCognome() + "!";
         benvenuto.setText(s);
+
+        listaPazienti.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                goListaPazienti();
+            }
+        });
+    }
+
+    public void goListaPazienti(){
+        Intent intent = new Intent(this, ListaPazienti.class);
+        startActivity(intent);
     }
 }
